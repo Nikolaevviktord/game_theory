dbe6e49aedd119a26ade0ef46d4e6c9574533c4d
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2024-10-21 15:41:50 +0300

    Update tit_for_two_tats.cpp - fixed bug

--- programs/tit_for_two_tats.cpp
+++ programs/tit_for_two_tats.cpp
@@ -1,6 +1,9 @@
 #include "data_struct.h"
 
 bool tit_for_two_tats(data_struct input_data) {
+	if (input_data.round_num <= 2)
+		return true;
+
 	bool last_sol = input_data.enemy_solutions.back();
 	bool prelast_sol = input_data.enemy_solutions[input_data.enemy_solutions.size() - 2];
 
@@ -9,4 +12,3 @@ bool tit_for_two_tats(data_struct input_data) {
 	else
 		return true;
 }
-
