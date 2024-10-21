cabf77623d561c002d733fb155536722e9a4da3f
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2024-10-21 15:41:23 +0300

    Update tit_for_tat.cpp - fixed bug

--- programs/tit_for_tat.cpp
+++ programs/tit_for_tat.cpp
@@ -1,6 +1,8 @@
 #include "data_struct.h"
 
 bool tit_for_tat(data_struct input_data) {
-	retrun input_data.enemy_solutions.back();
-}
+	if (input_data.round_num <= 1) 
+		return true;
 
+	return input_data.enemy_solutions.back();
+}
