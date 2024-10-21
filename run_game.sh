e63a413b68bc82640fa16c51182eb08ed93f9fe0
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2024-10-21 20:13:59 +0300

    Create run_game.sh

---
+++ run_game.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define variables for the files
+SOURCE_FILE="./programs/game.cpp"
+OUTPUT_FILE="./programs/game.out"
+RESULT_FILE="./result.txt"
+
+# Compile the C++ code
+g++ "$SOURCE_FILE" -o "$OUTPUT_FILE"
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+    # Execute the compiled program and save the output
+    "$OUTPUT_FILE" > "$RESULT_FILE"
+    echo "Program executed. Result saved in $RESULT_FILE"
+else
+    echo "Compilation error. Please check the code in $SOURCE_FILE"
+fi
